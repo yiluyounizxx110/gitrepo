e3cbf336fea4f5449c17ecca10d3ff9118f75189
xingxing.zhu <yiluyouni_zxx@sina.com>
2017-06-28 18:10:25 +0800

    提交

--- mingren/js/index.js
+++ mingren/js/index.js
@@ -1,5 +1,6 @@
 $(function(){
 	initPage();
+	initLabel();
 });
 function initPage(){
 	
@@ -7,7 +8,7 @@ function initPage(){
 	
 	$(".carousel_item").width(width);
 	$(".carousel_container").removeClass("none");
-	
+	//轮播初始化
 	slider.init();
 	
 	$(".carousel-indicator .carousel-indicators_item").mouseover(function(){
@@ -22,18 +23,134 @@ function initPage(){
 	$('.header_select_link').hover(function(){
 		var $fixBox=$('.fixBox');
     	$fixBox.css({width:$(window).width(),height:$('.carousel_item img').first().height()});
-        $('.fixBox').stop().slideDown(300);
+        $('.fixBox').stop().slideDown(400);
     },function(){
-        $('.fixBox').stop().slideUp(300,function(){
-            $(this).find("*").removeAttr('style');
+        $('.fixBox').stop().slideUp(400,function(){
+			$('.fixBox').css("display",'none')
         });
     });
+}
+
+function initLabel(){
+	var data={
+        code:0,
+        data:[
+            {
+                name:"职业标签",
+                data:[
+                    {
+                        name:"农业",
+                        data:["aaasfd","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321"]
+                    },{
+                        name:"林业",
+                        data:["aaasfd","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321"]
+                    },{
+                        name:"钢铁业",
+                        data:["aaasfd","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321"]
+                    },{
+                        name:"农业2",
+                        data:["aaasfd","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321"]
+                    },{
+                        name:"农业3",
+                        data:["aaasfd","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321"]
+                    },{
+                        name:"农业4",
+                        data:["aaasfd","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321"]
+                    },{
+                        name:"农业5",
+                        data:["aaasfd","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321"]
+                    }
+                ]
+            },{
+                name:"兴趣标签",
+                data:[
+                    {
+                        name:"电子",
+                        data:["aaasfd","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321"]
+                    },{
+                        name:"饮食",
+                        data:["aaasfd","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321"]
+                    },{
+                        name:"服装",
+                        data:["aaasfd","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321"]
+                    },{
+                        name:"玩具",
+                        data:["aaasfd","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321"]
+                    },{
+                        name:"电影",
+                        data:["aaasfd","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321"]
+                    },{
+                        name:"小说",
+                        data:["aaasfd","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321"]
+                    },{
+                        name:"手工",
+                        data:["aaasfd","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321"]
+                    }
+                ]
+            },{
+                name:"随机标签",
+                data:[
+                    {
+                        name:"农业",
+                        data:["aaasfd","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321"]
+                    },{
+                        name:"林业",
+                        data:["aaasfd","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321"]
+                    },{
+                        name:"钢铁业",
+                        data:["aaasfd","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321"]
+                    },{
+                        name:"农业2",
+                        data:["aaasfd","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321"]
+                    },{
+                        name:"农业3",
+                        data:["aaasfd","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321"]
+                    },{
+                        name:"农业4",
+                        data:["aaasfd","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321"]
+                    },{
+                        name:"农业5",
+                        data:["aaasfd","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321","dsfas","klkl","12321"]
+                    }
+                ]
+            }
+        ]
+    };
+    if(data.code==0){
+        var $BNavH="<div class='BNav'>",_navCenter="";
+        for(var i=0;i<data.data.length;i++){
+            $BNavH+="<span>"+data.data[i].name+"</span>";
+            _navCenter+="<div class='navCenter'><div class='navName'>名称</div><div class='navCenterBox'><div class='navCenterList'>";
+            var navCenterListCh="";
+            for(var n=0;n<data.data[i].data.length;n++){
+                _navCenter+="<span>"+data.data[i].data[n].name+"</span>";
+                navCenterListCh+="<div class='navCenterListCenter'>";
+                for(var j=0;j<data.data[i].data[n].data.length;j++){
+                    navCenterListCh+="<i>"+data.data[i].data[n].data[j]+"</i>"
+                }
+                navCenterListCh+="</div>"
+
+            }
+            _navCenter+="</div>"+navCenterListCh+"</div></div>";
+        }
+        $BNavH+="<i></i></div>";
+        $('.fixCenter').html($BNavH+_navCenter);
+    }
     
-//	
-//	window.onresize=function(){
-//		slider.stop();
-//		slider.init();
-//	}
+    
+    var $body=$('body');
+    $body.on('click','.BNav span',function(){
+        var _this=$(this);
+        var i=_this.index();
+        _this.parent().hide();
+        $('.navCenter').eq(i).show().siblings('.navCenter').hide().end().find('.navName').text(_this.text());
+    }).on('click','.navCenterListCenter i',function(){
+        //console.log($(this).text())
+			//此处获取点击的标签
+    }).on('mouseenter','.navCenterList span',function(){
+        var i=$(this).index();
+        $(this).parent().siblings('.navCenterListCenter').eq(i).stop().slideDown(300).siblings('.navCenterListCenter').hide();
+    })
 }
 var slider = {
 	cur_index:1,
