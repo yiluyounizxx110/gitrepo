52cef92a09f1906c534e65179c5a5c3eda10867d
zhuxingxing <990722162@qq.com>
2017-06-29 02:18:12 +0800

    城市选择，登录注册

--- mingren/js/index.js
+++ mingren/js/index.js
@@ -1,6 +1,8 @@
 $(function(){
 	initPage();
 	initLabel();
+	initAreaSelect();
+	initLoginInfo();
 });
 function initPage(){
 	
@@ -19,16 +21,6 @@ function initPage(){
 	$(".carousel-indicator .carousel-indicators_item").mouseout(function(){
 		slider.init();
 	});
-	
-	$('.header_select_link').hover(function(){
-		var $fixBox=$('.fixBox');
-    	$fixBox.css({width:$(window).width(),height:$('.carousel_item img').first().height()});
-        $('.fixBox').stop().slideDown(400);
-    },function(){
-        $('.fixBox').stop().slideUp(400,function(){
-			$('.fixBox').css("display",'none')
-        });
-    });
 }
 
 function initLabel(){
@@ -134,9 +126,18 @@ function initLabel(){
             _navCenter+="</div>"+navCenterListCh+"</div></div>";
         }
         $BNavH+="<i></i></div>";
-        $('.fixCenter').html($BNavH+_navCenter);
+        $('.header_select_link .fixCenter').html($BNavH+_navCenter);
     }
     
+    $('.header_select_link').hover(function(){
+		var $fixBox=$('.header_select_link .fixBox');
+    	$fixBox.css({width:$(window).width(),height:$('.carousel_item img').first().height()});
+        $fixBox.stop().slideDown(300);
+    },function(){
+        $('.header_select_link .fixBox').stop().slideUp(300,function(){
+			$('.header_select_link .fixBox').css("display",'none')
+        });
+    });
     
     var $body=$('body');
     $body.on('click','.BNav span',function(){
@@ -145,13 +146,95 @@ function initLabel(){
         _this.parent().hide();
         $('.navCenter').eq(i).show().siblings('.navCenter').hide().end().find('.navName').text(_this.text());
     }).on('click','.navCenterListCenter i',function(){
-        //console.log($(this).text())
-			//此处获取点击的标签
+    	//此处获取点击的标签
+        console.log($(this).text())
     }).on('mouseenter','.navCenterList span',function(){
         var i=$(this).index();
         $(this).parent().siblings('.navCenterListCenter').eq(i).stop().slideDown(300).siblings('.navCenterListCenter').hide();
     })
 }
+
+function initAreaSelect(){
+	$('.area').hover(function(){
+		var $fixBox=$('.area .fixBox');
+    	$fixBox.css({width:$(window).width(),height:$('.carousel_item img').first().height()});
+        $fixBox.stop().slideDown(300);
+    },function(){
+        $('.area .fixBox').stop().slideUp(300,function(){
+			$('.area .fixBox').css("display",'none')
+        });
+    });
+    $.getJSON("js/area.json",function(data){
+    	var html = "";
+		if(data.length > 0){
+			for(var i = 0; i < data.length;i ++){
+				var section_warpper = "<div class='section_warpper'>";
+				var section = data[i];
+				section_warpper += "<div class='area_section' data-index='"+ section.id +"'>"+ section.name +"</div>"
+				
+				if(section.data != null && section.data.length > 0){
+					section_warpper += "<div class='province_wrapper_container'>";
+					
+					for(var j = 0; j < section.data.length; j ++){
+						var province_wrapper = "<div class='province_wrapper'>";
+						var province = section.data[j];
+						if(province.id == null){
+							province_wrapper += "<div class='area_province_empty'></div>";
+						}else{
+							province_wrapper += "<div class='area_province' data-index='" + province.id + "'>"+ province.name +"</div>";
+						}
+						
+						if(province.data != null && province.data.length > 0){
+							province_wrapper += "<div class='city_wrapper'>";
+							
+							for(var m = 0; m < province.data.length ; m ++){
+								province_wrapper += "<div class='city_item' data-index='"+ province.data[m].id +"'>" + province.data[m].name + "</div>"
+							}
+							
+							province_wrapper += "</div>"
+						}
+						
+						province_wrapper += "</div>";
+						
+						section_warpper += province_wrapper;
+					}
+					
+					section_warpper += "</div>"
+					section_warpper += "<div class='clear'></div>"
+				}
+				
+				section_warpper += "</div>";
+				html += section_warpper;
+			}
+		}
+		$('.area .fixCenter').html(html);
+		var area_id = $(".area_text").attr("data-index");
+    	$(".area .fixCenter [data-index='" + area_id + "']").addClass("current");
+	})
+    
+}
+
+function initLoginInfo(){
+	$('.unlogin').hover(function(){
+		var $fixBox=$('.unlogin .fixBox');
+    	$fixBox.css({width:$(window).width(),height:$('.carousel_item img').first().height()});
+        $fixBox.stop().slideDown(300);
+        $fixBox.show();
+    },function(){
+        $('.unlogin .fixBox').stop().slideUp(300,function(){
+			$('.unlogin .fixBox').css("display",'none')
+        });
+    });
+    $("body").on("click",'.login_form .reg_btn',function(){
+    	$(".login_form ").addClass("none");
+    	$(".reg_form ").removeClass("none");
+    });
+    $("body").on("click",'.reg_form .login_btn',function(){
+    	$(".reg_form ").addClass("none");
+    	$(".login_form ").removeClass("none");
+    });
+}    
+
 var slider = {
 	cur_index:1,
 	sliderEvent:null,
@@ -176,3 +259,4 @@ var slider = {
 		$($(".carousel-indicator .carousel-indicators_item").get(index-1)).addClass('cur').siblings().removeClass('cur');
 	}
 }
+
